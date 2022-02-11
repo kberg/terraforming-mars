72c84741033b876a7eab25f2bfd6408bb25720a8
waichuan <nwai1990@hotmail.com>
2022-02-11 08:47:15 +0800

    Fix: CEO VP should count for Frontrunner

--- src/milestones/arabiaTerra/Frontrunner.ts
+++ src/milestones/arabiaTerra/Frontrunner.ts
@@ -1,3 +1,5 @@
+import {CardName} from "../../CardName";
+import {LeaderCard} from "../../cards/LeaderCard";
 import {Player} from "../../Player";
 import {IMilestone} from "../IMilestone";
 
@@ -17,6 +19,14 @@ export class Frontrunner implements IMilestone {
     // Victory points from corporation
     if (player.corporationCard !== undefined && player.corporationCard.getVictoryPoints !== undefined) {
       score += player.corporationCard.getVictoryPoints(player);
+    }
+
+    // Victory points from CEO
+    if (player.cardIsInEffect(CardName.DUNCAN)) {
+      const card = player.playedCards.find((c) => c.name === CardName.DUNCAN) as LeaderCard;
+      if (card.isDisabled === true && card.generationUsed !== undefined) {
+        score += 6 - card.generationUsed;
+      }
     }
 
     return score;
