4a5ce25c6961cec6d80e15dec6c53d23184ff930
waichuan <nwai1990@hotmail.com>
2023-03-08 14:41:19 +0800

    Implement CARP for Plantation

--- src/cards/base/Plantation.ts
+++ src/cards/base/Plantation.ts
@@ -8,8 +8,16 @@ import {ISpace} from '../../boards/ISpace';
 import {CardName} from '../../CardName';
 import {CardRequirements} from '../CardRequirements';
 import {CardRenderer} from '../render/CardRenderer';
+import {HowToAffordRedsPolicy, ActionDetails, RedsPolicy} from '../../turmoil/RedsPolicy';
+import {REDS_RULING_POLICY_COST} from '../../constants';
+import {PartyHooks} from '../../turmoil/parties/PartyHooks';
+import {PartyName} from '../../turmoil/parties/PartyName';
+import {Units} from '../../Units';
+import {TileType} from '../../TileType';
 
 export class Plantation extends Card implements IProjectCard {
+  public howToAffordReds: HowToAffordRedsPolicy | undefined;
+
   constructor() {
     super({
       cardType: CardType.AUTOMATED,
@@ -39,12 +47,33 @@ export class Plantation extends Card implements IProjectCard {
       return false;
     }
 
+    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) {
+      this.reserveUnits = Units.adjustUnits(this.reserveUnits, {megacredits: trGain * REDS_RULING_POLICY_COST});
+      const actionDetails = this.getActionDetails(player, this);
+      this.howToAffordReds = RedsPolicy.canAffordRedsPolicy(player, player.game, actionDetails);
+
+      if (this.howToAffordReds.mustSpendAtMost !== undefined || this.howToAffordReds.bonusMCFromPlay !== undefined) {
+        this.reserveUnits = Units.maybeAdjustReservedMegacredits(player, this.reserveUnits, this.howToAffordReds);
+      }
+
+      return this.howToAffordReds.canAfford;
+    }
+
     return true;
   }
 
   public play(player: Player) {
     return new SelectSpace('Select space for greenery tile', player.game.board.getAvailableSpacesForGreenery(player), (space: ISpace) => {
       return player.game.addGreenery(player, space.id);
+    });
+  }
+
+  public getActionDetails(player: Player, card: IProjectCard) {
+    return new ActionDetails({
+      card: card,
+      oxygenIncrease: 1,
+      nonOceanToPlace: TileType.GREENERY,
+      nonOceanAvailableSpaces: player.game.board.getAvailableSpacesForGreenery(player),
     });
   }
 }
