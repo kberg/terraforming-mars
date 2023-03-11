8a94bf4128675675f4bf2a0ceb22fbbf3455dde5
waichuan <nwai1990@hotmail.com>
2023-03-11 15:09:07 +0800

    Implement CARP for VenusMagnetizer

--- src/cards/venusNext/VenusMagnetizer.ts
+++ src/cards/venusNext/VenusMagnetizer.ts
@@ -10,8 +10,12 @@ import {PartyName} from '../../turmoil/parties/PartyName';
 import {CardRequirements} from '../CardRequirements';
 import {CardRenderer} from '../render/CardRenderer';
 import {Card} from '../Card';
+import {HowToAffordRedsPolicy, ActionDetails, RedsPolicy} from '../../turmoil/RedsPolicy';
+import {Units} from '../../Units';
 
 export class VenusMagnetizer extends Card implements IActionCard {
+  public howToAffordReds: HowToAffordRedsPolicy | undefined;
+
   constructor() {
     super({
       name: CardName.VENUS_MAGNETIZER,
@@ -35,19 +39,48 @@ export class VenusMagnetizer extends Card implements IActionCard {
   public play() {
     return undefined;
   }
+
   public canAct(player: Player): boolean {
     const venusMaxed = player.game.getVenusScaleLevel() === MAX_VENUS_SCALE;
     const hasEnergyProduction = player.getProduction(Resources.ENERGY) > 0;
+    const redsAreRuling = PartyHooks.shouldApplyPolicy(player, PartyName.REDS);
 
-    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) {
-      return player.canAfford(REDS_RULING_POLICY_COST) && hasEnergyProduction && !venusMaxed;
+    const trGain = this.getTotalTRGain(player);
+    Card.setRedsActionWarningText(trGain, this, redsAreRuling, 'raise Venus');
+
+    if (!hasEnergyProduction) return false;
+    if (venusMaxed) return true;
+
+    if (redsAreRuling) {
+      this.reserveUnits = Units.adjustUnits(this.reserveUnits, {megacredits: trGain * REDS_RULING_POLICY_COST});
+      const actionDetails = this.getActionDetails();
+      this.howToAffordReds = RedsPolicy.canAffordRedsPolicy(player, player.game, actionDetails);
+
+      if (this.howToAffordReds.mustSpendAtMost !== undefined || this.howToAffordReds.bonusMCFromPlay !== undefined) {
+        this.reserveUnits = Units.maybeAdjustReservedMegacredits(player, this.reserveUnits, this.howToAffordReds);
+      }
+
+      return this.howToAffordReds.canAfford;
     }
 
-    return hasEnergyProduction && !venusMaxed;
+    return true;
   }
+
   public action(player: Player) {
     player.addProduction(Resources.ENERGY, -1);
     player.game.increaseVenusScaleLevel(player, 1);
     return undefined;
+  }
+
+  private getTotalTRGain(player: Player): number {
+    const venusScale = player.game.getVenusScaleLevel();
+    let trGain = venusScale === MAX_VENUS_SCALE ? 0 : 1;
+    if (venusScale === 14) trGain += 1;
+
+    return trGain;
+  }
+
+  public getActionDetails() {
+    return new ActionDetails({venusIncrease: 1});
   }
 }
