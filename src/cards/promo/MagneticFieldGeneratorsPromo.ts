5bf7f84f102893c495dad2fe07a5e071a44f70ac
waichuan <nwai1990@hotmail.com>
2023-03-16 17:25:27 +0800

    Implement CARP for MagneticFieldGeneratorsPromo

--- src/cards/promo/MagneticFieldGeneratorsPromo.ts
+++ src/cards/promo/MagneticFieldGeneratorsPromo.ts
@@ -10,8 +10,14 @@ import {TileType} from '../../TileType';
 import {ISpace} from '../../boards/ISpace';
 import {CardRenderer} from '../render/CardRenderer';
 import {Units} from '../../Units';
+import {HowToAffordRedsPolicy, ActionDetails, RedsPolicy} from '../../turmoil/RedsPolicy';
+import {REDS_RULING_POLICY_COST} from '../../constants';
+import {PartyHooks} from '../../turmoil/parties/PartyHooks';
+import {PartyName} from '../../turmoil/parties/PartyName';
 
 export class MagneticFieldGeneratorsPromo extends Card implements IProjectCard {
+  public howToAffordReds: HowToAffordRedsPolicy | undefined;
+
   constructor() {
     super({
       cardType: CardType.AUTOMATED,
@@ -34,17 +40,32 @@ export class MagneticFieldGeneratorsPromo extends Card implements IProjectCard {
       },
     });
   }
-  public canPlay(player: Player): boolean {
-    if (!super.canPlay(player)) return false;
 
+  public canPlay(player: Player): boolean {
     const trGain = player.computeTerraformRatingBump(this);
     Card.setRedsWarningText(trGain, this);
 
     const meetsEnergyRequirements = player.getProduction(Resources.ENERGY) >= 4;
     const canPlaceTile = player.game.board.getAvailableSpacesOnLand(player).length > 0;
 
-    return meetsEnergyRequirements && canPlaceTile;
+    if (!meetsEnergyRequirements) return false;
+    if (!canPlaceTile) return false;
+
+    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) {
+      this.reserveUnits = Units.adjustUnits(this.reserveUnits, {megacredits: trGain * REDS_RULING_POLICY_COST});
+      const actionDetails = this.getActionDetails(player, this);
+      this.howToAffordReds = RedsPolicy.canAffordRedsPolicy(player, player.game, actionDetails, true);
+
+      if (this.howToAffordReds.mustSpendAtMost !== undefined || this.howToAffordReds.bonusMCFromPlay !== undefined) {
+        this.reserveUnits = Units.maybeAdjustReservedMegacredits(player, this.reserveUnits, this.howToAffordReds);
+      }
+
+      return this.howToAffordReds.canAfford;
+    }
+
+    return true;
   }
+
   public play(player: Player) {
     player.addProduction(Resources.ENERGY, -4);
     player.addProduction(Resources.PLANTS, 2);
@@ -57,5 +78,9 @@ export class MagneticFieldGeneratorsPromo extends Card implements IProjectCard {
       player.game.addTile(player, foundSpace.spaceType, foundSpace, {tileType: TileType.MAGNETIC_FIELD_GENERATORS});
       return undefined;
     });
+  }
+
+  public getActionDetails(_player: Player, card: IProjectCard) {
+    return new ActionDetails({card: card, TRIncrease: 3});
   }
 }
