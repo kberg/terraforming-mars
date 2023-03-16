8c60551ea8b73454a6790d953b61bc39a806ab38
waichuan <nwai1990@hotmail.com>
2023-03-16 22:21:37 +0800

    Implement CARP for AristarchusRoadNetwork

--- src/cards/moon/AristarchusRoadNetwork.ts
+++ src/cards/moon/AristarchusRoadNetwork.ts
@@ -10,8 +10,15 @@ import {MoonCard} from './MoonCard';
 import {AltSecondaryTag} from '../render/CardRenderItem';
 import {Card} from '../Card';
 import {MoonExpansion} from '../../moon/MoonExpansion';
+import {HowToAffordRedsPolicy, ActionDetails, RedsPolicy} from '../../turmoil/RedsPolicy';
+import {IProjectCard} from '../IProjectCard';
+import {REDS_RULING_POLICY_COST} from '../../constants';
+import {PartyHooks} from '../../turmoil/parties/PartyHooks';
+import {PartyName} from '../../turmoil/parties/PartyName';
 
 export class AristarchusRoadNetwork extends MoonCard {
+  public howToAffordReds: HowToAffordRedsPolicy | undefined;
+
   constructor() {
     super({
       name: CardName.ARISTARCHUS_ROAD_NETWORK,
@@ -46,6 +53,18 @@ export class AristarchusRoadNetwork extends MoonCard {
     const trGain = player.computeTerraformRatingBump(this);
     Card.setRedsWarningText(trGain, this);
 
+    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) {
+      this.reserveUnits = Units.adjustUnits(this.reserveUnits, {megacredits: trGain * REDS_RULING_POLICY_COST});
+      const actionDetails = this.getActionDetails(player, this);
+      this.howToAffordReds = RedsPolicy.canAffordRedsPolicy(player, player.game, actionDetails);
+
+      if (this.howToAffordReds.mustSpendAtMost !== undefined || this.howToAffordReds.bonusMCFromPlay !== undefined) {
+        this.reserveUnits = Units.maybeAdjustReservedMegacredits(player, this.reserveUnits, this.howToAffordReds);
+      }
+
+      return this.howToAffordReds.canAfford;
+    }
+
     return true;
   }
 
@@ -53,5 +72,9 @@ export class AristarchusRoadNetwork extends MoonCard {
     super.play(player);
     player.game.defer(new PlaceMoonRoadTile(player));
     return undefined;
+  }
+
+  public getActionDetails(_player: Player, card: IProjectCard) {
+    return new ActionDetails({card: card, moonLogisticsRateIncrease: 1});
   }
 }
