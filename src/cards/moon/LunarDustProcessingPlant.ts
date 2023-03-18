4023239d44274b383f3320c460b957e4d180b006
waichuan <nwai1990@hotmail.com>
2023-03-18 14:06:12 +0800

    Implement CARP for LunarDustProcessingPlant

--- src/cards/moon/LunarDustProcessingPlant.ts
+++ src/cards/moon/LunarDustProcessingPlant.ts
@@ -8,8 +8,15 @@ import {CardRenderer} from '../render/CardRenderer';
 import {TileType} from '../../TileType';
 import {Units} from '../../Units';
 import {MoonCard} from './MoonCard';
+import {HowToAffordRedsPolicy, ActionDetails, RedsPolicy} from '../../turmoil/RedsPolicy';
+import {REDS_RULING_POLICY_COST} from '../../constants';
+import {PartyHooks} from '../../turmoil/parties/PartyHooks';
+import {PartyName} from '../../turmoil/parties/PartyName';
+import {Card} from '../Card';
 
 export class LunarDustProcessingPlant extends MoonCard implements IProjectCard {
+  public howToAffordReds: HowToAffordRedsPolicy | undefined;
+
   constructor() {
     super({
       name: CardName.LUNAR_DUST_PROCESSING_PLANT,
@@ -17,6 +24,7 @@ export class LunarDustProcessingPlant extends MoonCard implements IProjectCard {
       tags: [Tags.BUILDING],
       cost: 6,
       reserveUnits: Units.of({titanium: 1}),
+      tr: {moonLogistics: 1},
 
       metadata: {
         description: 'Spend 1 titanium. Raise the Logistic Rate 1 step.',
@@ -31,10 +39,33 @@ export class LunarDustProcessingPlant extends MoonCard implements IProjectCard {
     });
   };
 
+  public canPlay(player: Player) {
+    const trGain = player.computeTerraformRatingBump(this);
+    Card.setRedsWarningText(trGain, this);
+
+    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) {
+      this.reserveUnits = Units.adjustUnits(this.reserveUnits, {megacredits: trGain * REDS_RULING_POLICY_COST});
+      const actionDetails = this.getActionDetails(player, this);
+      this.howToAffordReds = RedsPolicy.canAffordRedsPolicy(player, player.game, actionDetails, true);
+
+      if (this.howToAffordReds.mustSpendAtMost !== undefined || this.howToAffordReds.bonusMCFromPlay !== undefined) {
+        this.reserveUnits = Units.maybeAdjustReservedMegacredits(player, this.reserveUnits, this.howToAffordReds);
+      }
+
+      return this.howToAffordReds.canAfford;
+    }
+
+    return true;
+  }
+
 
   public play(player: Player) {
     super.play(player);
     MoonExpansion.raiseLogisticRate(player);
     return undefined;
+  }
+
+  public getActionDetails(_player: Player, card: IProjectCard) {
+    return new ActionDetails({card: card, moonLogisticsRateIncrease: 1});
   }
 }
