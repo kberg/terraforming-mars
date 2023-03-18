4ac7d443b9fb8d468226ca2aeb74cc59ce0e5056
waichuan <nwai1990@hotmail.com>
2023-03-18 20:39:00 +0800

    Implement CARP for OffWorldCityLiving

--- src/cards/moon/OffWorldCityLiving.ts
+++ src/cards/moon/OffWorldCityLiving.ts
@@ -8,8 +8,15 @@ import {MoonExpansion} from '../../moon/MoonExpansion';
 import {CardRenderer} from '../render/CardRenderer';
 import {CardRenderDynamicVictoryPoints} from '../render/CardRenderDynamicVictoryPoints';
 import {Card} from '../Card';
+import {HowToAffordRedsPolicy, ActionDetails, RedsPolicy} from '../../turmoil/RedsPolicy';
+import {REDS_RULING_POLICY_COST} from '../../constants';
+import {PartyHooks} from '../../turmoil/parties/PartyHooks';
+import {PartyName} from '../../turmoil/parties/PartyName';
+import {Units} from '../../Units';
 
 export class OffWorldCityLiving extends Card implements IProjectCard {
+  public howToAffordReds: HowToAffordRedsPolicy | undefined;
+
   constructor() {
     super({
       name: CardName.OFF_WORLD_CITY_LIVING,
@@ -33,10 +40,20 @@ export class OffWorldCityLiving extends Card implements IProjectCard {
   };
 
   public canPlay(player: Player): boolean {
-    if (!super.canPlay(player)) return false;
-
     const trGain = player.computeTerraformRatingBump(this);
     Card.setRedsWarningText(trGain, this);
+
+    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) {
+      this.reserveUnits = Units.adjustUnits(this.reserveUnits, {megacredits: trGain * REDS_RULING_POLICY_COST});
+      const actionDetails = this.getActionDetails(player, this);
+      this.howToAffordReds = RedsPolicy.canAffordRedsPolicy(player, player.game, actionDetails, false, true);
+
+      if (this.howToAffordReds.mustSpendAtMost !== undefined || this.howToAffordReds.bonusMCFromPlay !== undefined) {
+        this.reserveUnits = Units.maybeAdjustReservedMegacredits(player, this.reserveUnits, this.howToAffordReds);
+      }
+
+      return this.howToAffordReds.canAfford;
+    }
 
     return true;
   }
@@ -51,5 +68,9 @@ export class OffWorldCityLiving extends Card implements IProjectCard {
   public getVictoryPoints(player: Player) {
     const amount = player.game.getCitiesInPlay();
     return Math.floor(amount / 3);
+  }
+
+  public getActionDetails(_player: Player, card: IProjectCard) {
+    return new ActionDetails({card: card, moonColonyRateIncrease: 1});
   }
 }
