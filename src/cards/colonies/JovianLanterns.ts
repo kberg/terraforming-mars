747ac7886a0d02c752346d5c7f879c07ce616148
waichuan <nwai1990@hotmail.com>
2023-04-06 16:08:18 +0800

    Add missing req check for JovianLanterns

--- src/cards/colonies/JovianLanterns.ts
+++ src/cards/colonies/JovianLanterns.ts
@@ -43,6 +43,8 @@ export class JovianLanterns extends Card implements IProjectCard, IResourceCard 
   public resourceCount: number = 0;
 
   public canPlay(player: Player): boolean {
+    if (!super.canPlay(player)) return false;
+
     const trGain = player.computeTerraformRatingBump(this);
     Card.setRedsWarningText(trGain, this);
 
