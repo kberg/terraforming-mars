6a6f57aaac6646390fb324d1dc08be63458d74db
waichuan <nwai1990@hotmail.com>
2023-08-16 08:53:32 +0800

    Fix more Italian translations

--- src/cards/base/LavaFlows.ts
+++ src/cards/base/LavaFlows.ts
@@ -29,7 +29,7 @@ export class LavaFlows extends Card implements IProjectCard {
         b.temperature(2).br;
         b.tile(TileType.LAVA_FLOWS, true, false).asterix();
       }),
-      description: 'Raise temperature 2 steps and place this tile on a VOLCANIC AREA',
+      description: 'Raise temperature 2 steps and place this tile on a VOLCANIC AREA.',
     }) {
     super({
       cardType: CardType.EVENT,
