4092f52a7028e56efedb7e049eed71adc21e07d8
waichuan <nwai1990@hotmail.com>
2023-09-12 19:43:17 +0800

    Update Worker to not count preludes with actions

--- src/awards/arabiaTerra/Worker.ts
+++ src/awards/arabiaTerra/Worker.ts
@@ -1,4 +1,5 @@
 import {Player} from "../../Player";
+import {CardType} from "../../cards/CardType";
 import {getAdditionalScore, IAward} from "../IAward";
 
 export class Worker implements IAward {
@@ -6,7 +7,7 @@ export class Worker implements IAward {
   public description: string = 'Have the most active (blue) action cards'
   
   public getScore(player: Player): number {
-    let score = player.playedCards.filter((card) => card.action !== undefined).length;
+    let score = player.playedCards.filter((card) => card.cardType !== CardType.PRELUDE).filter((card) => card.action !== undefined).length;
 
     player.corporationCards.forEach((corp) => {
       if (corp.action !== undefined) score += 1;
