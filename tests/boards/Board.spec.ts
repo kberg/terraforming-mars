9139b88e3250fdeb701b048163a3872b979b6287
Robert Konigsberg <konigsberg@gmail.com>
2024-04-07 17:34:37 -0400

    Move noctis city calcuation to Board and MarsBoard.

--- tests/boards/Board.spec.ts
+++ tests/boards/Board.spec.ts
@@ -294,7 +294,7 @@ describe('Board', function() {
 
   class TestBoard extends Board {
     public constructor(spaces: Array<Space>) {
-      super(spaces);
+      super(spaces, undefined);
     }
   }
 
